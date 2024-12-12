5ac461f9257ae134cfa848dcf953d4e240645bc2
FrancisOwusu <osei1257@gmail.com>
2024-12-12 19:44:34 +0000

    refactor to dashboard using suspense

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,14 @@
-import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { lusitana } from '@/app/ui/fonts';
- import { fetchLatestInvoices,fetchCardData } from '../lib/data';
- import { Suspense } from 'react';
- import { RevenueChartSkeleton } from '../ui/skeletons';
+import { Card } from "@/app/ui/dashboard/cards";
+import  CardWrapper  from "@/app/ui/dashboard/cards";
+// import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+import { fetchLatestInvoices, fetchCardData } from "../lib/data";
+import { Suspense } from "react";
+import { RevenueChartSkeleton, LatestInvoicesSkeleton,CardSkeleton } from "../ui/skeletons";
+import RevenueChart from "../ui/dashboard/revenue-chart";
+// import { Card } from "../ui/dashboard/cards";
+
 export default async function Page() {
   // const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
@@ -24,29 +28,27 @@ export default async function Page() {
         {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
         {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
         {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-         <Card
+        {/* <Card
           title="Total Customers"
           value={cardData.numberOfCustomers}
           type="customers"
-        />
-        
+        /> */}
       </div>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
         {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
         {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-         <Card
+        <Card
           title="Total Invoices"
           value={cardData.numberOfInvoices}
           type="invoices"
         />
-        
       </div>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
         {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
         {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-         <Card
+        <Card
           title="Total Pending Invoices"
           value={cardData.totalPendingInvoices}
           type="pending"
@@ -56,7 +58,7 @@ export default async function Page() {
         {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
         {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
         {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-         <Card
+        <Card
           title="Total Paid Invoices"
           value={cardData.totalPaidInvoices}
           type="collected"
@@ -65,9 +67,14 @@ export default async function Page() {
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         {/* <RevenueChart revenue={revenue}  /> */}
         <Suspense fallback={<RevenueChartSkeleton />}>
-         <RevenueChart revenue={[]} />
+          <RevenueChart revenue={[]} />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices latestInvoices={[]} />
+        </Suspense>
+        <Suspense fallback={<CardWrapper />}>
+        </Suspense>
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>
   );
